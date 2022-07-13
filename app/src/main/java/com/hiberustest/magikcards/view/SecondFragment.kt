53f896a42eb336bc0cd814ded534cf85e5ec28ac
Iryna Klitna <iryna.klitna@nttdata.com>
2022-07-13 04:03:23 +0200

    view package added

--- app/src/main/java/com/hiberustest/magikcards/view/SecondFragment.kt
+++ app/src/main/java/com/hiberustest/magikcards/view/SecondFragment.kt
@@ -1,4 +1,4 @@
-package com.hiberustest.magikcards
+package com.hiberustest.magikcards.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.hiberustest.magikcards.R
 import com.hiberustest.magikcards.databinding.FragmentSecondBinding
 
 /**
